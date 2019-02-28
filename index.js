3b93181bdac6b1edd76842bee40d88014776a6c6
Carl Hjerpe <github@lillecarl.com>
2019-02-28 10:40:08 +0100

    Rewrite using a nice recorder that does DOM selections way better than i do.

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 const puppeteer = require('puppeteer');
-const ping = require('ping');
 
-ping.sys.probe('8.8.8.8', function(isAlive) {
-  (async () => {
-    if (!isAlive) {
-      console.log('Sending SMS to Tele2');
-      const browser = await puppeteer.launch({headless: false});
-      const page = await browser.newPage();
-      await page.goto('http://' + process.env.IP);
-      await page.waitForSelector('#txtPwd');
-      await page.type('#txtPwd', 'process.env.PASS', {delay: 100});
-      await page.click('#btnLogin');
-      await page.waitFor(2500);
-      await page.goto('http://192.168.100.1/index.html#sms');
-      await page.waitForSelector('#smslist-new-sms');
-      await page.click('#smslist-new-sms');
-      await page.waitForSelector('#chosen-search-field-input');
-      await page.type('#chosen-search-field-input', '772661', {delay: 100});
-      await page.type('#chosen-search-field-input', String.fromCharCode(13));
-      await page.type('#chat-input', 'SNOOZE');
-      await page.click('#btn-send');
-      await page.waitFor(2500);
-      //browser.close()
-    }
-    else {
-      console.log('Already got data');
-    }
-  })(); 
-})
+if (!process.env.IP) {
+  console.log("Need to set environment variable IP");
+}
+if (!process.env.PASS) {
+  console.log("Need to set environment variable PASS");
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.pages().then((x) => {
+    return x[0]
+  });
+
+  await page.goto('http://' + process.env.IP + '/index.html#login')
+
+  await page.setViewport({
+    width: 1440,
+    height: 701
+  })
+
+  await page.waitForSelector('#loginContainer > #frmLogin > .row > .col-xs-4 > #btnLogin')
+  await page.type('#txtPwd', process.env.PASS)
+  await page.click('#loginContainer > #frmLogin > .row > .col-xs-4 > #btnLogin')
+
+  await page.waitForSelector('#container > .row > a:nth-child(2) > .col-xs-2 > .center-block')
+  await page.click('#container > .row > a:nth-child(2) > .col-xs-2 > .center-block')
+
+  await page.waitForSelector('.table > #smslist-table > #smslist-item-72661 > .cursorhand > .sms-table-content')
+  await page.click('.table > #smslist-table > #smslist-item-72661 > .cursorhand > .sms-table-content')
+
+  await page.waitForSelector('#smsChatRoom > #inputpanel > .chatform > .chattextinput > #chat-input')
+
+  page.waitFor(500);
+
+  const data = await page.evaluate(() => {
+    const tds = Array.from(document.querySelectorAll('.J_content'))
+    return tds.map(td => td.textContent)
+  })
+
+  var lastsms = data[Math.max(data.length - 1, 0)];
+
+  if (lastsms.includes("SNOOZE") && lastsms.includes("Tele2")) {
+    console.log("Sending SMS");
+    await page.type('#chat-input', 'SNOOZE');
+    await page.click('#smsChatRoom > #inputpanel > .chatform > .chattextinput > #chat-input')
+
+    await page.waitForSelector('.chatform > .chatfun > .col-xs-4 > span > #btn-send')
+    await page.click('.chatform > .chatfun > .col-xs-4 > span > #btn-send')
+  } else {
+    console.log("Not sending SMS");
+  }
+
+  await page.waitFor(5000);
+  await browser.close()
+})()
